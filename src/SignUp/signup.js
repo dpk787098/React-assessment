74b612b4b82e78d2fe7bcb55eec83f276a278db3
dpk787098 <deepak787098@gmail.com>
2021-03-16 18:58:42 +0000

    first one

--- src/SignUp/signup.js
+++ src/SignUp/signup.js
@@ -10,12 +10,13 @@ class SignUp extends Component {
         return ( 
             <form className="signup_form_wrapper">
                 <h2>SIGN UP</h2>
-                <input required type="text" placeholder="First Name" />
-                <input required type="text" placeholder="Last Name" />
-                <input required type="email" placeholder="Email" />
-                <input required type="password" placeholder="Password" />
-                <input required type="password" placeholder="Confirm Password" />
-                <button type="submit">SignUp</button>
+                <p>**Was not asked to implemented. So disabled</p>
+                <input disabled required type="text" placeholder="First Name" />
+                <input disabled required type="text" placeholder="Last Name" />
+                <input disabled required type="email" placeholder="Email" />
+                <input disabled required type="password" placeholder="Password" />
+                <input disabled required type="password" placeholder="Confirm Password" />
+                <button disabled type="submit">SignUp</button>
             </form>
          );
     }
